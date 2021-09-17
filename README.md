d39a94ee1f60d28b3c9970f71307a003905f67cd
Burak <burakkepuc.1996@gmail.com>
2021-09-18 00:11:08 +0300

    Write readme and Javascript add

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 ## ROCK PAPER SCISSORS GAME
 
-[] There are 3 objects ,rock paper and scissors.
-[] There are 2 oppenent, human human,computer human or computer computer.
-[] Start the game,choose your object and let your opponent chose its object at the same time.
-[]  -Rock beats Scissors, 
+[x] There are 3 objects rock, paper and scissors.
+[x] There are 2 oppenent, human human,computer human or computer computer.
+[x] Start the game,choose your object and let your opponent chose its object at the same time.
+[x]  -Rock beats Scissors, 
     -Paper beats Rock
     -Scissors beats Paper
-[] -If both same, it's draw, for example rock vs rock.
+[x] -If both same, it's draw, for example rock vs rock.
 [] When the game over,show the screen that who is won.
+
+# Functions
+[x] Starts with computerPlay function that will return ‘Rock’, ‘Paper’ or ‘Scissors’
+[x] Write a function that plays a single round of 
+‘Rock’, ‘Paper’ or ‘Scissors’. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+ -Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-console.log("Hello")
+let rock = "Rock",
+    paper = "Paper",
+    scissors = "Scissors";
+
+let playerSelection;
+let computerSelection;
+
+function computerPlay() {
+
+    let rand = Math.floor(Math.random() * 3 + 1);
+
+    if (rand == 1) {
+        return rock;
+    } else if (rand == 2) {
+        return paper;
+    } else {
+        return scissors;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    playerSelection = prompt("Choose (Rock,Paper or Scissors)");
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    computerSelection = computerPlay();
+       
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            return (`You lose! Computer won! You:${playerSelection} Computer:${computerSelection}`);
+        } else if (computerSelection === "Scissors") {
+            return (`You won! Computer lost. You:${playerSelection} Computer:${computerSelection}`);
+        } else {
+            return (`Draw! You:${playerSelection} computer:${computerSelection}`);
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            return (`You won! Computer lost. You:${playerSelection} Computer:${computerSelection}`);
+        } else if (computerSelection === "Scissors") {
+            return (`You lose! Computer won! You:${playerSelection} Computer:${computerSelection}`);
+        } else {
+            return (`Draw! You:${playerSelection} computer:${computerSelection}`);
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Paper") {
+            return (`You won! Computer lost. You:${playerSelection} Computer:${computerSelection}`);
+        } else if (computerSelection === "Rock") {
+            return (`You lose! Computer won! You:${playerSelection} Computer:${computerSelection}`);
+        } else {
+            return (`Draw! You:${playerSelection} Computer:${computerSelection}`);
+        }
+    } else {
+        return "Invalid character!";
+    }
+}
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
